e2d60c380ec71ad3303793b7deffd6dc2308a4f4
neresdu <eduardo.neres@hitechnologies.com.br>
2022-07-21 18:47:14 -0300

    Removing unused stuffs

--- CRUD_Payrolls/src/controllers/PayrollController.ts
+++ CRUD_Payrolls/src/controllers/PayrollController.ts
@@ -78,41 +78,4 @@ export class PayrollController {
 
   };
 
-  // calcular(request: Request, response: Response) {
-  //   var payrolls = PayrollSchema.find( {processed: false});
-  //   console.log(payrolls)
-  //   // const folhasProcessadas = folhaPagamentoRepository.listarProcessadas();
-  //     axios
-  //     .post("http://localhost:3334/folha/cadastrar", payrolls )
-  //     .then((response) => {
-  //     console.log(response);
-  //    })
-  //     .catch((error) => {  
-  //     console.log(error);
-  //     });
-
-    
-  //   response.status(200).json({ message: "Listagem de produtos", data: payrolls });
-  // }
-  listar(request: Request, response: Response) {
-    var payrolls = PayrollSchema.find( {processed: false})
-    .exec()
-    .then((results)=> {
-      return response.status(201).json({
-        payrolls: results,
-        count: results.length
-      });
-    })
-    .catch(error => {
-      return response.status(500).json({
-        message: error.message,
-        error
-      });
-    });
-  };
-
-  listarProcessadas(request: Request, response: Response) {
-    var payrolls = PayrollSchema.find( {processed: true});
-    response.status(200).json({ message: "Listagem de produtos", data: payrolls });
-  }
 }
